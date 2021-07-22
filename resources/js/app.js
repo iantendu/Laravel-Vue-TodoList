ad97d749fa6e6716eb235ba8222f9355c461d5c6
iantendu <iantendu@gmail.com>
2021-07-22 17:41:17 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require("./bootstrap");
+
+import Vue from "vue";
+import App from "./vue/app";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPlusSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPlusSquare, faTrash);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+const app = new Vue({
+    el: "#app",
+    components: { App }
+});
